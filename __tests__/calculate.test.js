bc8f8c894352305f406ece204a09a6e7b84afec9
mtaylorpdx <taylor.matt@protonmail.com>
2020-02-07 13:51:55 -0800

    add test for calculating remaining planet years of life based on user input life expectation

--- __tests__/calculate.test.js
+++ __tests__/calculate.test.js
@@ -11,8 +11,7 @@ describe ('Person', () => {
     expect(person.planetAges(37)).toEqual([154.17, 59.68, 19.68, 3.12]);
   });
 
-  test('should calculate life expectancy on each planet based on user expectations', ()=> {
-    
-
+  test('should calculate remaining life expectancy on each planet based on user input', ()=> {
+    expect(person.mortality(72)).toEqual([145.83, 56.45, 18.62, 2.95]);
   });
 });
--- src/calculate.js
+++ src/calculate.js
@@ -11,5 +11,4 @@ export class Person {
     console.log (ages);
     return (ages);
   }
-
 }
