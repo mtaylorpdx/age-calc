4c342f8420713d2118f0fb0204ed035224322445
mtaylorpdx <taylor.matt@protonmail.com>
2020-02-07 10:02:14 -0800

    add test for mercury years

--- __tests__/calulate.test.js
+++ __tests__/calulate.test.js
@@ -12,4 +12,9 @@ describe ('Person', () => {
     person.age = ageInput;
     expect(person.age).toEqual(37);
   });
+
+  test('should return age in Mercury years rounded to 2 decimals, where 1 year equals 0.24 Earth years', ()=> {
+    let ageInput = 37;
+    expect(person.mercury(person.age)).toEqual(154.16);
+  });
 });
--- src/calculate.js
+++ src/calculate.js
@@ -1,5 +1,5 @@
 export class Person {
   constructor() {
-    
+    this.age = 0;
   }
 }
