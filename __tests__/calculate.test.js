ebce86ff23be652dbdd2d666b2803ddbd06d9ee9
mtaylorpdx <taylor.matt@protonmail.com>
2020-02-07 12:19:52 -0800

    refactor separate planet prototypes into one

--- __tests__/calculate.test.js
+++ __tests__/calculate.test.js
@@ -7,22 +7,6 @@ describe ('Person', () => {
     person = new Person(37);
   });
 
-  test('should return age in Mercury years rounded to 2 decimals, where 1 year equals 0.24 Earth years', ()=> {
-    expect(person.mercury()).toEqual(154.17);
-  });
-
-  test('should return age in Venus years rounded to 2 decimals, where 1 year equals 0.62 Earth years', ()=> {
-    expect(person.venus()).toEqual(59.68);
-  });
-
-  test('should return age in Mars years rounded to 2 decimals, where 1 year equals 1.88 Earth years', ()=> {
-    expect(person.mars()).toEqual(19.68);
-  });
-
-  test('should return age in Jupiter years rounded to 2 decimals, where 1 year equals 11.86 Earth years', ()=> {
-    expect(person.jupiter()).toEqual(3.12);
-  });
-
   test('should return ages for all planets', ()=> {
     expect(person.planetAges(37)).toEqual([154.17, 59.68, 19.68, 3.12]);
   });
--- src/calculate.js
+++ src/calculate.js
@@ -11,28 +11,4 @@ export class Person {
     console.log (ages);
     return (ages);
   }
-
-  mercury(){
-    let mercuryAge = 0;
-    mercuryAge = parseFloat((this.age / 0.24).toFixed(2));
-    return mercuryAge;
-  }
-
-  venus(){
-    let venusAge = 0;
-    venusAge = parseFloat((this.age / 0.62).toFixed(2));
-    return venusAge;
-  }
-
-  mars(){
-    let marsAge = 0;
-    marsAge = parseFloat((this.age / 1.88).toFixed(2));
-    return marsAge;
-  }
-
-  jupiter(){
-    let jupiterAge = 0;
-    jupiterAge = parseFloat((this.age / 11.86).toFixed(2));
-    return jupiterAge;
-  }
 }
