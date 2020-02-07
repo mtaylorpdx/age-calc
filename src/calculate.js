2c12376fe9b269f219bc2649ab4f775ac0b88afb
mtaylorpdx <taylor.matt@protonmail.com>
2020-02-07 12:18:57 -0800

    refactor separate planet prototypes into one

--- src/calculate.js
+++ src/calculate.js
@@ -3,11 +3,18 @@ export class Person {
     this.age = age;
   }
 
+  planetAges(age){
+    const planetYears = [0.24, 0.62, 1.88, 11.86];
+    let ages = planetYears.map(function(ratio) {
+      return parseFloat((age / ratio).toFixed(2));
+    })
+    console.log (ages);
+    return (ages);
+  }
+
   mercury(){
     let mercuryAge = 0;
-    console.log(this.age);
     mercuryAge = parseFloat((this.age / 0.24).toFixed(2));
-    console.log(mercuryAge);
     return mercuryAge;
   }
 
