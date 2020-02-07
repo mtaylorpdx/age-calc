6fbe593276c31cc4f52713d1edb1ff3565b134ea
mtaylorpdx <taylor.matt@protonmail.com>
2020-02-07 15:18:07 -0800

    add input form with button

--- src/calculate.js
+++ src/calculate.js
@@ -1,19 +1,28 @@
 export class Person {
   constructor(age) {
     this.age = age;
+    this.ages = [];
+    this.agesRemaining = [];
+    this.agesExceeded = [];
   }
 
   planetAges(age){
     const planetYears = [0.24, 0.62, 1.88, 11.86];
     let ages = planetYears.map(function(ratio) {
       return parseFloat((age / ratio).toFixed(2));
-    })
+    });
     return ages;
   }
 
   mortality(deathAge){
-    let remaining = deathAge - this.age;
-    let ages = this.planetAges(remaining);
-    return ages;
+    if (this.age > deathAge) {
+      let remaining = this.age - deathAge;
+      let agesExceeded = this.planetAges(remaining);
+      return agesExceeded;
+    } else {
+      let remaining = deathAge - this.age;
+      let agesRemaining = this.planetAges(remaining);
+      return agesRemaining;
+    }
   }
 }
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { Person } from './calculate';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+$(document).ready(function() {
+  $("#compute").click(function(event) { 
+    event.preventDefault();
+    console.log(person);
+
+  });
+
+
+});
