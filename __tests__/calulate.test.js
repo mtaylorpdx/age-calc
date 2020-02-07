8085b51a647dbe67f385a898c017d34490e12e63
mtaylorpdx <taylor.matt@protonmail.com>
2020-02-07 10:50:23 -0800

    add failing test for mars years

--- __tests__/calulate.test.js
+++ __tests__/calulate.test.js
@@ -27,4 +27,12 @@ describe ('Person', () => {
   test('should return age in Venus years rounded to 2 decimals, where 1 year equals 0.62 Earth years', ()=> {
     expect(person.venus()).toEqual(59.68);
   });
+
+  test('should return age in Mars years rounded to 2 decimals, where 1 year equals 1.88 Earth years', ()=> {
+    expect(person.mars()).toEqual(19.68);
+  });
+
+  // test('should return age in Jupiter years rounded to 2 decimals, where 1 year equals 11.86 Earth years', ()=> {
+  //   expect(person.jupiter()).toEqual(3.12);
+  // });
 });
