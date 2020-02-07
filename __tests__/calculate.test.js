4d70dfebcfe0ba97717f11aeff161817d4fd6722
mtaylorpdx <taylor.matt@protonmail.com>
2020-02-07 14:15:12 -0800

    add test for how long past life expectancy

--- __tests__/calculate.test.js
+++ __tests__/calculate.test.js
@@ -14,4 +14,8 @@ describe ('Person', () => {
   test('should calculate remaining life expectancy on each planet based on user input', ()=> {
     expect(person.mortality(72)).toEqual([145.83, 56.45, 18.62, 2.95]);
   });
+
+  test('should determine how long past life expectancy if applicable', ()=> {
+    expect(person.mortality(35)).toEqual([8.33, 3.23, 1.06, 0.17]);
+  });
 });
