12d23d45d22051a68d9a6fb9209a2aa27b88147d
mtaylorpdx <taylor.matt@protonmail.com>
2020-02-07 18:07:19 -0800

    make html changes

--- __tests__/calculate.test.js
+++ __tests__/calculate.test.js
@@ -8,14 +8,19 @@ describe ('Person', () => {
   });
 
   test('should return ages for all planets', ()=> {
-    expect(person.planetAges(37)).toEqual([154.17, 59.68, 19.68, 3.12]);
+    person.planetAges(37);
+    expect(person.ages).toEqual([154.17, 59.68, 19.68, 3.12]);
   });
 
   test('should calculate remaining life expectancy on each planet based on user input', ()=> {
-    expect(person.mortality(72)).toEqual([145.83, 56.45, 18.62, 2.95]);
+    person.planetAges(37);
+    person.mortality(72, 37);
+    expect(person.agesRemaining).toEqual([145.83, 56.45, 18.62, 2.95]);
   });
 
   test('should determine how long past life expectancy if applicable', ()=> {
-    expect(person.mortality(35)).toEqual([8.33, 3.23, 1.06, 0.17]);
+    person.planetAges(37);
+    person.mortality(35, 37);
+    expect(person.agesExceeded).toEqual([8.33, 3.23, 1.06, 0.17]);
   });
 });
--- src/calculate.js
+++ src/calculate.js
@@ -2,6 +2,8 @@ export class Person {
   constructor(age) {
     this.age = age;
     this.ages = [];
+    this.agesExceeded = [];
+    this.agesRemaining = [];
   }
 
   planetAges(age) {
@@ -9,18 +11,18 @@ export class Person {
     let ages = planetYears.map(function(ratio) {
       return parseFloat((age / ratio).toFixed(2));
     });
-    return ages;
+    this.ages = ages;
   }
 
-  mortality(deathAge) {
-    if (this.age > deathAge) {
-      let remaining = this.age - deathAge;
-      let agesExceeded = this.planetAges(remaining);
-      return agesExceeded;
+  mortality(deathAge, age) {
+    if (age > deathAge) {
+      let remaining = age - deathAge;
+      let agesExceeded = age.planetAges(remaining);
+      this.agesExceeded = agesExceeded;
     } else {
       let remaining = deathAge - this.age;
-      let agesRemaining = this.planetAges(remaining);
-      return agesRemaining;
+      let agesRemaining = age.planetAges(remaining);
+      this.agesRemaining = agesRemaining;
     }
   }
 }
