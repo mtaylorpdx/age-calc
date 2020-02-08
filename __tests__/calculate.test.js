a922b7d8550d1e83f478d1669717de49c1bd032c
mtaylorpdx <taylor.matt@protonmail.com>
2020-02-07 18:14:25 -0800

    revert code back to before refactoring into an array

--- __tests__/calculate.test.js
+++ __tests__/calculate.test.js
@@ -1,26 +1,58 @@
-import { Person } from './../src/calculate.js';
+export class Person {
+  constructor(age) {
+    this.age = age;
+  }
 
-describe ('Person', () => {
-  let person;
+  mercury(){
+    let mercuryAge = 0;
+    console.log(this.age);
+    mercuryAge = parseFloat((this.age / 0.24).toFixed(2));
+    console.log(mercuryAge);
+    return mercuryAge;
+  }
 
-  beforeEach( ()=> {
-    person = new Person(37);
-  });
+  venus(){
+    let venusAge = 0;
+    venusAge = parseFloat((this.age / 0.62).toFixed(2));
+    return venusAge;
+  }
 
-  test('should return ages for all planets', ()=> {
-    person.planetAges(37);
-    expect(person.ages).toEqual([154.17, 59.68, 19.68, 3.12]);
-  });
+  mars(){
+    let marsAge = 0;
+    marsAge = parseFloat((this.age / 1.88).toFixed(2));
+    return marsAge;
+  }
 
-  test('should calculate remaining life expectancy on each planet based on user input', ()=> {
-    person.planetAges(37);
-    person.mortality(72, 37);
-    expect(person.agesRemaining).toEqual([145.83, 56.45, 18.62, 2.95]);
-  });
+  jupiter(){
+    let jupiterAge = 0;
+    jupiterAge = parseFloat((this.age / 11.86).toFixed(2));
+    return jupiterAge;
+  }
+}
 
-  test('should determine how long past life expectancy if applicable', ()=> {
-    person.planetAges(37);
-    person.mortality(35, 37);
-    expect(person.agesExceeded).toEqual([8.33, 3.23, 1.06, 0.17]);
-  });
-});
+// import { Person } from './../src/calculate.js';
+
+// describe ('Person', () => {
+//   let person;
+
+//   beforeEach( ()=> {
+//     person = new Person(37);
+//   });
+
+//   test('should return ages for all planets', ()=> {
+//     person.planetAges(37);
+//     expect(person.ages).toEqual([154.17, 59.68, 19.68, 3.12]);
+//   });
+
+//   test('should calculate remaining life expectancy on each planet based on user input', ()=> {
+//     person.planetAges(37);
+//     person.mortality(72, 37);
+//     expect(person.agesRemaining).toEqual([145.83, 56.45, 18.62, 2.95]);
+//   });
+
+//   test('should determine how long past life expectancy if applicable', ()=> {
+//     person.planetAges(37);
+//     person.mortality(35, 37);
+//     expect(person.agesExceeded).toEqual([8.33, 3.23, 1.06, 0.17]);
+//   });
+// });
--- src/calculate.js
+++ src/calculate.js
@@ -1,28 +1,58 @@
-export class Person {
-  constructor(age) {
-    this.age = age;
-    this.ages = [];
-    this.agesExceeded = [];
-    this.agesRemaining = [];
-  }
+import { Person } from './../src/calculate.js';
 
-  planetAges(age) {
-    const planetYears = [0.24, 0.62, 1.88, 11.86];
-    let ages = planetYears.map(function(ratio) {
-      return parseFloat((age / ratio).toFixed(2));
-    });
-    this.ages = ages;
-  }
+describe ('Person', () => {
+  let person;
 
-  mortality(deathAge, age) {
-    if (age > deathAge) {
-      let remaining = age - deathAge;
-      let agesExceeded = age.planetAges(remaining);
-      this.agesExceeded = agesExceeded;
-    } else {
-      let remaining = deathAge - this.age;
-      let agesRemaining = age.planetAges(remaining);
-      this.agesRemaining = agesRemaining;
-    }
-  }
-}
+  beforeEach( ()=> {
+    person = new Person(37);
+  });
+
+  test('should return age in Mercury years rounded to 2 decimals, where 1 year equals 0.24 Earth years', ()=> {
+    expect(person.mercury()).toEqual(154.17);
+  });
+
+  test('should return age in Venus years rounded to 2 decimals, where 1 year equals 0.62 Earth years', ()=> {
+    expect(person.venus()).toEqual(59.68);
+  });
+
+  test('should return age in Mars years rounded to 2 decimals, where 1 year equals 1.88 Earth years', ()=> {
+    expect(person.mars()).toEqual(19.68);
+  });
+
+  test('should return age in Jupiter years rounded to 2 decimals, where 1 year equals 11.86 Earth years', ()=> {
+    expect(person.jupiter()).toEqual(3.12);
+  });
+
+  test('should return ages for all planets', ()=> {
+    expect(person.planetAges()).toEqual([154.17, 59.68, 19.68, 3.12]);
+  });
+});
+
+// export class Person {
+//   constructor(age) {
+//     this.age = age;
+//     this.ages = [];
+//     this.agesExceeded = [];
+//     this.agesRemaining = [];
+//   }
+
+//   planetAges(age) {
+//     const planetYears = [0.24, 0.62, 1.88, 11.86];
+//     let ages = planetYears.map(function(ratio) {
+//       return parseFloat((age / ratio).toFixed(2));
+//     });
+//     this.ages = ages;
+//   }
+
+//   mortality(deathAge, age) {
+//     if (age > deathAge) {
+//       let remaining = age - deathAge;
+//       let agesExceeded = age.planetAges(remaining);
+//       this.agesExceeded = agesExceeded;
+//     } else {
+//       let remaining = deathAge - this.age;
+//       let agesRemaining = age.planetAges(remaining);
+//       this.agesRemaining = agesRemaining;
+//     }
+//   }
+// }
